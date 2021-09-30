ecd6fcfe28f69f19e96e9ac72640c7d1a4a10510
Steve <stautonico@gmail.com>
2021-09-30 04:14:44 -0400

    Testing code for starting personal blog

---
+++ client/src/pages/blog/[title].svelte
@@ -0,0 +1,21 @@
+<script>
+
+    import marked from "marked";
+
+    export let title;
+    let renderedMarkdown = "";
+
+    fetch(`/api/${title}.md`)
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+        data = data.replace(/(---\n.*\n---)/gims, "");
+        renderedMarkdown = marked(data);
+    })
+
+
+</script>
+
+<div>
+    {@html renderedMarkdown}
+</div>
---
+++ client/src/pages/blog/index.svelte
@@ -0,0 +1,3 @@
+<div>
+    <h1>All blog posts go here:</h1>
+</div>
