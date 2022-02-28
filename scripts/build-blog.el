9300fb7512d0686df40c3e34111b2a7ccb17d300
Steve <stautonico@gmail.com>
2022-02-28 02:35:32 -0500

    Added build script for emacs

---
+++ scripts/build-blog.el
@@ -0,0 +1,18 @@
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
+(use-package ox-hugo
+  :ensure t   ;Auto-install the package from Melpa
+  :pin melpa  ;`package-archives' should already have ("melpa" . "https://melpa.org/packages/")
+  :after ox)
+
+(org-hugo-export-wim-to-md t)
